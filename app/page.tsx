f9509504c17fa9ecb84432f350f7b0837ed6d827
Do Tien Minh <minh@example.com>
2026-03-23 16:21:11 +0700

    loi commen

--- app/page.tsx
+++ app/page.tsx
@@ -315,7 +315,7 @@ function HomeContent() {
                   {/* ĐÂY CHÍNH LÀ THẺ QUAN TRỌNG NHẤT MÀ BẠN ĐANG THIẾU */}
                   <div 
                     className="fb-comments" 
-                    data-href="https://developers.facebook.com/docs/plugins/comments/"
+                    data-href={typeof window !== 'undefined' ? window.location.href : 'https://allinonevn.com'}
                     data-width="100%" 
                     data-numposts="10"
                     data-order-by="reverse_time"
