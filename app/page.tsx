bd8ce0094b90326660635f2e5db513def065d390
Do Tien Minh <minh@example.com>
2026-03-23 10:40:23 +0700

    fix commen

--- app/page.tsx
+++ app/page.tsx
@@ -297,7 +297,7 @@ function HomeContent() {
             </div>
           </div>
         )}
-        {/* TRANG CỘNG ĐỒNG & FEEDBACK */}
+
         {/* TRANG CỘNG ĐỒNG & FEEDBACK */}
         {activeTab === 'community' && (
            <div className="space-y-12 animate-fade-in mt-10">
@@ -313,7 +313,7 @@ function HomeContent() {
                   {/* ĐÂY CHÍNH LÀ THẺ QUAN TRỌNG NHẤT MÀ BẠN ĐANG THIẾU */}
                   <div 
                     className="fb-comments" 
-                    data-href="https://cua-hang-cua-minh.vercel.app" 
+                    data-href="https://developers.facebook.com/docs/plugins/comments/"
                     data-width="100%" 
                     data-numposts="10"
                     data-order-by="reverse_time"
